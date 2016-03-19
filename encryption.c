bec697e33efe5dc71416ea299ce682bb5eb4e59b
benjamin d ward <benstopics@gmail.com>
2016-03-19 19:33:47 -0400

    MANUALLY WROTE OUT HASH TABLE

--- encryption.c
+++ encryption.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 
+unsigned char hashTable[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
 unsigned char *Base2ToBase64(unsigned char *base2)
 {
 	unsigned char *result;
-	unsigned char firstLetter[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
-	
 	
 }
 
