12f528899117b64798196e18be0b43361bc983bc
Bill Nicholson <nicholdw@ucmail.uc.edu>
2016-03-22 09:29:49 -0400

    Added programmatic analysis in Challenge 1 Set 3
    It's index 88 I think.

--- set1challenge3.c
+++ set1challenge3.c
@@ -6,32 +6,50 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "encryption.h"
+
+long analyze(char *tmp);
+
 
 unsigned char *set1challenge3(unsigned char *msg) {
 	size_t size = GetHexSizeOfStr(msg);
 	unsigned char *result = malloc(size + 1); // +1 for null terminator
 	unsigned char *hexData = HexToBase2(msg);
 	unsigned char j;
-	unsigned int i, k;
+	unsigned int i, k, highScoreIdx = 0;
 	unsigned char highNibble, lowNibble;
 	unsigned char ASCIIChar;
+	long score, highScore = -1;
 
 	for (i = 0; i < 256; i++) {
 		// XOR the original string with a constant value
 		unsigned char *tmp = malloc(size + 1); // +1 for null terminator
 		*(tmp + size) = 0; // null terminator
-		printf("%u ==> ", i);
+		//printf("%u ==> ", i);
 		for (j = 0; j < size; j++) {
 			*(tmp + j) = *(hexData + j) ^ i;
-			if (*(tmp + j) == 0) // Convert false null to space for printing
-				*(tmp + j) = 32;
-			printf("%c", *(tmp + j));
-		} printf("\n");
+			//if (*(tmp + j) == 0) // Convert false null to space for printing
+			//	*(tmp + j) = 32;
+			//printf("%c", *(tmp + j));
+		}
+		score = 0;
+		//if (i == 89)
+		score = analyze(tmp); 
+
+		if (score > highScore) { 
+			highScore = score;
+			highScoreIdx = i;
+			printf("\n new high score = %ld  at index %d ==> ", score, i);
+			for (j = 0; j < size; j++) {
+				printf("%c", *(tmp + j));
+			}
+
+		}
 
 		// Eye-balled it
-		if (i == 120)
-			result = tmp;
+		//if (i == 120)
+		//	result = tmp;
 
 		/*k = 0;
 		// Convert the newly created string to ASCII characters. We assume that the newly created string contains 2-byte ASCII hex codes.
@@ -44,7 +62,50 @@ unsigned char *set1challenge3(unsigned char *msg) {
 		}*/
 		//printf("\n %d <%s>", i, tmp, size);
 	}
+	//printf("\n High Score was index %d", highScoreIdx);
 	return result;
+}
+
+long analyze(char *tmp) {
+	long score = 0;
+	int scoreArray[26];	// a bucket for each letter in the alphabet
+	int i, idx, controlCharacterCount = 0, spaceCount = 0;;
+	unsigned char letter;
+	for (i = 0; i < 26; i++) scoreArray[i] = 0;
+
+	// Count the instances of each letter in the english alphabet
+	for (i = 0; *(tmp + i) != 0; i++) {
+		letter = *(tmp + i);
+		if (isspace(letter)) {
+			spaceCount++;
+		} else {
+			if (iscntrl(letter)) { 
+				controlCharacterCount++; 
+			} else {
+				letter = toupper(*(tmp + i));
+				if (letter >= 'A' && letter <= 'Z') {
+					idx = letter - 'A';
+					scoreArray[idx]++;
+				}
+			}
+		}
+	}
+	// r,s,t,l,n,e occur most often according to Wheel of Fortune TV show
+	// See also https://en.wikipedia.org/wiki/Letter_frequency
+	score += scoreArray['R' - 'A'];
+	score += scoreArray['S' - 'A'];
+	score += scoreArray['T' - 'A'];
+	score += scoreArray['L' - 'A'];
+	score += scoreArray['N' - 'A'];
+	score += scoreArray['A' - 'A'];
+	score += scoreArray['E' - 'A'];
+	score += scoreArray['I' - 'A'];
+	score += scoreArray['O' - 'A'];
+	score += scoreArray['U' - 'A'];
+
+	if (controlCharacterCount > 0) score = 0;
+	if (spaceCount < 5) score = 0;
+	return score;
 }
 
 int ASCIIhexToBinary(unsigned char digit) {
@@ -68,3 +129,4 @@ int ASCIIhexToBinary(unsigned char digit) {
 	}
 	return digit;
 }
+
