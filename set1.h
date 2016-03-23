5260e8b6ca74cecd1019afe7529b76b46ddaeac6
Bill Nicholson <nicholdw@ucmail.uc.edu>
2016-03-22 21:54:56 -0400

    Set 1 Challenge 4 started

--- set1.h
+++ set1.h
@@ -1,5 +1,19 @@
+/**************************************
+  * Set 1 header               set1.h *
+  * Bill Nicholson                    *
+  * nicholdw@ucmail.uc.edu            *
+  * Ben Ward                          *
+  * wardb4@mail.uc.edu                *
+  *************************************/
+ 
+#ifndef __SET1__
+#define __SET1__
 void set1challenge1();
 
 void set1challenge2();
 
 unsigned char *set1challenge3(unsigned char *msg);
+
+
+void set1challenge4();
+#endif
--- set1Challenge4.c
+++ set1Challenge4.c
@@ -15,16 +15,25 @@ Find it.
 #include "encryption.h"
 #include "set1.h"
 
-#define DATAFILE "setChallenge4.txt"
+#define DATAFILE "set1Challenge4.txt"
+static long analyze(char *tmp);
+static void processText(char *msg);
 
-void setChallenge5() {
-	FILE *handle = fopen(DATAFILE, "r");
-	char * buffer[80];
+void set1Challenge4() {
+	FILE *handle;
+	fopen_s(&handle, DATAFILE, "r");
+	char buffer[80];
+	int i = 0;
 
 	if (handle != NULL) {
 		while (fgets(buffer, 80, handle) != 0) {
-			printf("\n processing %s", buffer);
+			
+			printf("\n %d: processing %s", i, buffer);
+			processText(buffer);
 
+
+
+			i++;
 		}
 
 	} else {
@@ -33,4 +42,62 @@ void setChallenge5() {
 	}
 
 
+}
+static void processText(char *msg) {
+	size_t size = GetHexSizeOfStr(msg);
+	long score, highScore = -1;
+	unsigned char *hexData = HexToBase2(msg);
+	unsigned int i, j, highScoreIdx = 0;
+	for (i = 0; i < 256; i++) {
+		// XOR the original string with a constant value
+		unsigned char *tmp = malloc(size + 1); // +1 for null terminator
+		*(tmp + size) = 0; // null terminator
+		//printf("%u ==> ", i);
+		for (j = 0; j < size; j++) {
+			*(tmp + j) = *(hexData + j) ^ i;
+			//if (*(tmp + j) == 0) // Convert false null to space for printing
+			//	*(tmp + j) = 32;
+			//printf("%c", *(tmp + j));
+		}
+		score = 0;
+		score = analyze(tmp);
+		if (score > highScore) {
+			highScore = score;
+			highScoreIdx = i;
+			printf("\n new high score = %ld  at index %d ==> ", score, i);
+			for (j = 0; j < size; j++) {
+				printf("%c", *(tmp + j));
+			}
+
+		}
+	}
+}
+
+static long analyze(char *tmp) {
+	long score = 0;
+	int scoreArray[26];	// a bucket for each letter in the alphabet
+	int i, idx, controlCharacterCount = 0, spaceCount = 0;;
+	unsigned char letter;
+	for (i = 0; i < 26; i++) scoreArray[i] = 0;
+
+	// Count the instances of each letter in the english alphabet
+	for (i = 0; *(tmp + i) != 0; i++) {
+		letter = *(tmp + i);
+		if (isspace(letter)) {
+			spaceCount++;
+		}
+		else {
+			if (iscntrl(letter)) {
+				controlCharacterCount++;
+			}
+			else {
+				letter = toupper(*(tmp + i));
+				if (letter >= 'A' && letter <= 'Z') {
+					idx = letter - 'A';
+					scoreArray[idx]++;
+				}
+			}
+		}
+	}
+	return score;
 }
